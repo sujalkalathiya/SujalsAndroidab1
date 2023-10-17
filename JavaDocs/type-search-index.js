9b1db30631e1a26af21926d3af85075b3db56c10
sujalkalathiya <kala0049@algonquinlive.com>
2023-10-17 04:48:10 -0400

    Added JavaDoc Comments and added 5 test cases

---
+++ JavaDocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"algonquin.cst2335.kala0049","l":"ExampleInstrumentedTest"},{"p":"algonquin.cst2335.kala0049","l":"ExampleUnitTest"},{"p":"algonquin.cst2335.kala0049","l":"MainActivity"}];updateSearchResults();
